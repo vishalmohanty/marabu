70a9b5fa6b688053d00a9be776fa48c9850ac84e
Vishal Mohanty <vishalmohanty97@gmail.com>
2023-01-16 20:16:54 +0000

    Add timeouts for invalid messages

---
+++ src/temp.ts
@@ -0,0 +1,21 @@
+import {Socket} from "net";
+
+// The port number and hostname of the server.
+const PORT = 18018;
+const hostIP = "149.28.223.2"
+
+// Create a new TCP client.
+const client = new Socket();
+// Send a connection request to the server.
+client.connect({ port: PORT, host: hostIP }, function() {
+    // If there is no error, the server has accepted the request and created a new
+    // socket dedicated to us.
+    console.log('TCP connection established with the server with IP address ' + hostIP);
+
+    // The client can now send data to the server by writing to its socket.
+    client.write('{"type": "hello", "agent": "Marabu-Core Client 0.9", "version": "0.9.3"}');
+
+    // setTimeout(() => {
+    //         client.write('\n');
+    // }, 5000);
+});
--- src/util/json_defragmenter.ts
+++ src/util/json_defragmenter.ts
@@ -1,13 +1,21 @@
 import { Socket } from "net";
 import { ErrorMessage } from "../protocol/messaging/messages/error";
 
+const TIMEOUT : number = 10000 // Timeout to receive a valid message
+
 class JSONDefragmenter {
     buffer : string;
     socket : Socket;
+    counter : number;
+    timeoutID : NodeJS.Timeout;
 
     constructor(socket : Socket) {
         this.buffer = ""
         this.socket = socket
+        // Counter helps us keep track of the fragment of message. 
+        // Timer is started only on the first character of a new message
+        this.counter = 0
+        this.timeoutID = null
     }
     *feed(buf : Buffer) {
         for(const val of buf) {
@@ -18,7 +26,23 @@ class JSONDefragmenter {
                     (new ErrorMessage(this.socket, "INVALID_FORMAT", "Not parsable as json.")).send()
                 }
                 this.buffer = ""
+                // Reset the timer when we get any fully formed message (terminated by '\n')
+                clearTimeout(this.timeoutID);
+                this.counter = 0;
             } else {
+                if (this.counter == 0) {
+                    this.counter += 1
+                    // Start a timer on receiving the first fragment. On timing out, close connection
+                    this.timeoutID = setTimeout(() => {
+                        (
+                            new ErrorMessage(this.socket, "INVALID_FORMAT", "Timed out waiting to receive a valid message.")).send(() => {
+                                console.log(`Timed out waiting to receive a valid message. Terminated connection with ${this.socket.remoteAddress}`)
+                                this.socket.destroy()
+                                return null
+                            }
+                        )
+                    }, TIMEOUT);
+                }
                 this.buffer += String.fromCharCode(val)
             }
         }
