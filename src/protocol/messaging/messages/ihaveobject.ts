871940a5c31cef95aac9f11f7f5b42aac22de5ce
Sudeep Narala <sudeeprnarala@gmail.com>
2023-01-24 07:47:48 +0000

    Fix ihaveobject

--- src/protocol/messaging/messages/ihaveobject.ts
+++ src/protocol/messaging/messages/ihaveobject.ts
@@ -1,7 +1,7 @@
 import { BlockchainState } from "../../state/blockchain_state";
 import {MarabuSocket} from "../../../util/marabu_socket"
 import { Message } from "../message_types/message";
-import { get_from_db } from "../../../util/database";
+import { exists_in_db, get_from_db } from "../../../util/database";
 import {create_get_object_message} from "./getobject"
 
 interface IHaveObject {
@@ -19,7 +19,7 @@ class IHaveObjectMessage extends Message {
     }
     async _perform_validated_receive() {
         // TODO: Request object if you don't have it.
-        get_from_db(this.obj.objectid).then((present) => {if(present) {create_get_object_message(this.socket, this.blockchain_state, this.obj.objectid).run_send_actions()}})
+        exists_in_db(this.obj.objectid).then((present) => {if(!present) {create_get_object_message(this.socket, this.blockchain_state, this.obj.objectid).run_send_actions()} else {console.log(`[ihaveobject] Object with id ${this.obj.objectid} exists in the DB.`)}})
     }
 
 }
